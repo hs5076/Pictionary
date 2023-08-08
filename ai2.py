11a2b2c56a9728c743290071079e15f8035a874a
hs5076 <141275765+hs5076@users.noreply.github.com>
2023-08-08 16:12:29 -0400

    Add files via upload

--- ai2.py
+++ ai2.py
@@ -24,8 +24,6 @@ os.chdir(script_directory)
 interpreter1 = tflite.Interpreter(model_path='model_unquant.tflite')
 interpreter2 = tflite.Interpreter(model_path='model.tflite')
 
-global vimage
-global correct
 vimage = random.choice(imagelist)
 if vimage == imagelist[0]:
 	correct = 'circle'
@@ -35,8 +33,7 @@ if vimage == imagelist[2]:
 	correct = 'triangle'
 print(correct)
 
-def modelpred(interpreter, classlist):
-	global vimage
+def modelpred(interpreter, classlist, minconf):
 	interpreter.allocate_tensors()
 	input_details = interpreter.get_input_details()
 	output_details = interpreter.get_output_details()
@@ -44,12 +41,10 @@ def modelpred(interpreter, classlist):
 	width = input_details[0]['shape'][2]
 	img = Image.open(vimage).resize((width, height))
 	img = img.convert('RGB')
-	if interpreter == interpreter1:
-		input_data = np.array(img, dtype=np.float32)  # Convert img to NumPy array
-		input_data = np.expand_dims(input_data / 255.0, axis=0) 
-	else:
-		input_data = np.array(img, dtype=np.uint8)
-		input_data = np.expand_dims(input_data, axis=0)
+	input_data = np.array(img).reshape(input_details[0]['shape'])/255.0 # reshape eliminates needs for expand_dims
+	if input_details[0]['quantization_parameters']['scales'].size > 0: # if it is quantized, divide by scaling parameter
+		input_data = np.array(input_data/input_details[0]['quantization_parameters']['scales'])
+	input_data = np.array(input_data, dtype = input_details[0]['dtype']) # either way, make sure dtype is correct
 	start_time = time.time()
 	interpreter.set_tensor(input_details[0]['index'], input_data)
 	interpreter.invoke()
@@ -57,48 +52,52 @@ def modelpred(interpreter, classlist):
 	time_total = time.time() - start_time
 	maximum = np.argmax(prediction)
 	class_name = classlist[maximum]
-	if interpreter == interpreter1:
-		confidence_score = prediction[0][maximum]
-	else:
-		confidence_score = prediction[0][maximum] / 255.0
-	if float(confidence_score) <= 0.65:
-		return['none', time_total]
+	confidence_score = prediction[0][maximum]
+	if output_details[0]['quantization_parameters']['scales'].size > 0:
+		confidence_score = confidence_score * output_details[0]['quantization_parameters']['scales']
+	if float(confidence_score) <= minconf:
+		return [None, time_total]
 	else:
 		return[class_name[2:].strip(), time_total]
 while True:
-	x = modelpred(interpreter1, class_names)
+	x = modelpred(interpreter1, class_names, 0.65)
 	print('Model 1 : ')
 	print(x)
-	y = modelpred(interpreter2, class_names2)
+	y = modelpred(interpreter2, class_names2, 0.65)
 	print('Model 2 : ')
 	print(y)
-	time.sleep(1)
 	if x[0] == correct and y[0] != correct:
 		print('Model 1 wins')
 		break
 	elif x[0] != correct and y[0] == correct:
 		print('Model 2 wins')
 		break
-	elif x[0] == correct and y[0] == correct:
-		if x[1] < y[1]:
-			print('Model 1 wins by time')
-			print( 'Model 1 time : ' + str(x[1]) + '\nModel 2 time : ' + str(y[1]))
-			break
-		else:
-			print('Model 2 wins by time')
-			print( 'Model 1 time : ' + str(x[1]) + '\nModel 2 time : ' + str(y[1]))
-			break
-	elif x[0] != correct and y[0] != correct:
-		if x[0] == 'none' and y[0] == 'none':
-			print('both have submitted none')
-			continue
-		elif x[0] == 'none' and y[0] != 'none':
-			z += 1
-		elif x[0] != 'none' and y[0] == 'none':
-			n += 1
-		elif n == 2:
-			print('Model 2 wins by forfeit')
-			break
-		elif z == 2:
-			print('Model 1 wins by forfeit')
-			break	
+	elif x[0] == correct and y[0] == correct and x[1] < y[1]:
+		print('Model 1 wins by time')
+		print( 'Model 1 time : ' + str(x[1]) + '\nModel 2 time : ' + str(y[1]))
+		break
+	elif x[0] == correct and y[0] == correct and x[1] > y[1]:
+		print('Model 2 wins by time')
+		print( 'Model 1 time : ' + str(x[1]) + '\nModel 2 time : ' + str(y[1]))
+		break
+	elif x[0] == correct and y[0] == correct and x[1] == y[1]:
+		print('Somehow its a tie! Trying again')
+		print( 'Model 1 time : ' + str(x[1]) + '\nModel 2 time : ' + str(y[1]))
+		continue
+	elif not x[0] and not y[0]:
+		print('both have submitted no answer')
+		continue
+	elif not x[0] and y[0]:
+		z += 1
+	elif x[0] and not y[0]:
+		n += 1
+	elif x[0] and y[0]:
+		z += 1
+		n += 1
+		print('Both have submitted a wrong guess')
+	if n == 2:
+		print('Model 2 wins by forfeit')
+		break
+	if z == 2:
+		print('Model 1 wins by forfeit')
+		break	
